a9d3c3e8f520b0d52957a0b567fcef2d667d22b0
iben polanco <iben.fpolanco@gmail.com>
2025-09-25 13:35:15 -0500

    Declarar todas las nuevas actividades en el AndroidManifest

--- PracticaComunicacion/app/src/main/java/com/example/practicacomunicacion/MainActivity.kt
+++ PracticaComunicacion/app/src/main/java/com/example/practicacomunicacion/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.practicacomunicacion
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.widget.Button
 
+/**
+ * Descripción: Actividad principal que funciona como un menú para lanzar los ejercicios.
+ * Autor: Iben Flores Polanco
+ * Fecha creación: 25/09/2025
+ * Fecha última modificación: 25/09/2025
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        findViewById<Button>(R.id.btnIrAPerfil).setOnClickListener {
+            startActivity(Intent(this, FormularioActivity::class.java))
+        }
+
+        findViewById<Button>(R.id.btnIrANota).setOnClickListener {
+            startActivity(Intent(this, EditorActivity::class.java))
         }
     }
 }
