43df98052220fb1de449d81871adbbd25d075f7e
iben polanco <iben.fpolanco@gmail.com>
2025-10-01 15:08:28 -0500

    Configurar MainActivity para alojar el NavHostFragment

--- NavegacionPedidos/app/src/main/java/com/example/navegacionpedidos/MainActivity.kt
+++ NavegacionPedidos/app/src/main/java/com/example/navegacionpedidos/MainActivity.kt
@@ -1,20 +1,17 @@
 package com.example.navegacionpedidos
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
 
+/**
+ * Descripción: Actividad principal que aloja el grafo de navegación.
+ * Autor: Iben Flores Polanco
+ * Fecha creación: 01/10/2025
+ * Fecha última modificación: 01/10/2025
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
